2dd3dd05a522ed6dcb237e6d274a10d1e1365d47
528814@escg.ac.uk <528814@escg.ac.uk>
2022-10-03 15:15:01 +0100

    new challenges 3/10/22

---
+++ Printing Variables/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        string hello;
+
+        void Hello()
+        {
+            hello = "hello";
+        }
+
+        Hello();
+
+        Console.WriteLine(hello);
+
+        int a = 1;
+        Console.WriteLine(a);
+
+        //whole number
+        int b = 2;
+
+        //decimal number
+        double d = 3.5;
+
+        //word
+        string f = "word";
+
+        //multiple numbers
+        int[] myNumbers = {1, 2, 3, 4, 5};
+        Console.WriteLine(myNumbers);
+    }
+}
---
+++ Programming Challenges 28.09.22/Program.cs
@@ -0,0 +1,13 @@
+﻿class Car
+{
+    public string color = "red";
+}
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Car myObj = new Car();
+        Console.WriteLine(myObj.color);
+    }
+}
