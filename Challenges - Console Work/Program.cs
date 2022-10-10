39ccf7985482693848ede72e554e6d22f27359c8
AlexH528814 <528814@escg.ac.uk>
2022-10-11 00:40:15 +0100

    added even or odd

--- Challenges - Console Work/Program.cs
+++ Challenges - Console Work/Program.cs
@@ -1,6 +1,4 @@
-﻿using System;
-
-class Program
+﻿class Program
 {
     static void Main()
     {
@@ -10,97 +8,87 @@ class Program
         int playerDamage3 = random.Next(5, 10);
         int playerDamage4 = random.Next(20, 25);
         int enemyDamage = random.Next(10, 15);
-
         int playerHealth = 150;
         int enemyHealth = 150;
 
-
+        bool attackmode = false;
 
         Console.WriteLine("You encountered an enemy, press C, R, Y or F to attack it");
+        attackmode = true;
 
-        answerfunc();
-
-        if (enemyHealth <= 0)
+        while (attackmode == true)
         {
-            Console.WriteLine("You defeated the enemy");
-            Environment.Exit(0);
-        }
+            if (enemyHealth <= 0)
+            {
+                Console.WriteLine("You defeated the enemy");
+                attackmode = false;
+                Environment.Exit(0);
+            }
 
-        if (playerHealth <= 0)
-        {
-            Console.WriteLine("The enemy defeated you");
-            Environment.Exit(0);
-        }
+            if (playerHealth <= 0)
+            {
+                Console.WriteLine("The enemy defeated you");
+                attackmode = false;
+                Environment.Exit(0);
+            }
 
+            var input = Console.ReadKey();
+            Console.WriteLine("");
+            switch (input.Key)
+            {
+                case ConsoleKey.C:
 
-    }
+                    enemyHealth = enemyHealth - playerDamage1;
+                    Console.WriteLine($"You attacked the enemy for {playerDamage1}");
+                    Console.WriteLine($"The enemy now has {enemyHealth} health");
+                    playerHealth = enemyHealth - enemyDamage;
+                    Console.WriteLine($"The enemy attacked you in retaliation, your health is now {playerHealth}");
+                    break;
 
+                case ConsoleKey.R:
 
-    static void answerfunc()
-    {
-        Random random = new Random();
-        int playerDamage1 = random.Next(10, 15);
-        int playerDamage2 = random.Next(15, 20);
-        int playerDamage3 = random.Next(5, 10);
-        int playerDamage4 = random.Next(20, 25);
-        int enemyDamage = random.Next(10, 15);
-        int playerHealth = 150;
-        int enemyHealth = 150;
+                    enemyHealth = enemyHealth - playerDamage2;
+                    Console.WriteLine($"You attacked the enemy for {playerDamage1}");
+                    Console.WriteLine($"The enemy now has {enemyHealth} health");
+                    playerHealth = enemyHealth - enemyDamage;
+                    Console.WriteLine($"The enemy attacked you in retaliation, your health is now {playerHealth}");
+                    break;
 
+                case ConsoleKey.Y:
 
-        var input = Console.ReadKey();
-        Console.WriteLine("");
-        switch (input.Key)
-        {
-            case ConsoleKey.C:
-                
-                enemyHealth = enemyHealth - playerDamage1;
-                Console.WriteLine($"You attacked the enemy for {playerDamage1}");
-                Console.WriteLine($"The enemy now has {enemyHealth} health");
-                playerHealth = enemyHealth - enemyDamage;
-                Console.WriteLine($"The enemy attacked you in retaliation, your health is now {playerHealth}");
-                answerfunc();
-                break;
+                    enemyHealth = enemyHealth - playerDamage3;
+                    Console.WriteLine($"You attacked the enemy for {playerDamage1}");
+                    Console.WriteLine($"The enemy now has {enemyHealth} health");
+                    playerHealth = enemyHealth - enemyDamage;
+                    Console.WriteLine($"The enemy attacked you in retaliation, your health is now {playerHealth}");
+                    break;
 
-            case ConsoleKey.R:
-               
-                enemyHealth = enemyHealth - playerDamage2;
-                Console.WriteLine($"You attacked the enemy for {playerDamage1}");
-                Console.WriteLine($"The enemy now has {enemyHealth} health");
-                playerHealth = enemyHealth - enemyDamage;
-                Console.WriteLine($"The enemy attacked you in retaliation, your health is now {playerHealth}");
-                answerfunc();
-                break;
+                case ConsoleKey.F:
 
-            case ConsoleKey.Y:
-                
-                enemyHealth = enemyHealth - playerDamage3;
-                Console.WriteLine($"You attacked the enemy for {playerDamage1}");
-                Console.WriteLine($"The enemy now has {enemyHealth} health");
-                playerHealth = enemyHealth - enemyDamage;
-                Console.WriteLine($"The enemy attacked you in retaliation, your health is now {playerHealth}");
-                answerfunc();
-                break;
+                    enemyHealth = enemyHealth - playerDamage4;
+                    playerHealth = enemyHealth - enemyDamage;
+                    Console.WriteLine($"You attacked the enemy for {playerDamage1}");
+                    Console.WriteLine($"The enemy now has {enemyHealth} health");
+                    Console.WriteLine($"The enemy attacked you in retaliation, your health is now {playerHealth}");
+                    break;
 
-            case ConsoleKey.F:
-              
-                enemyHealth = enemyHealth - playerDamage4;
-                Console.WriteLine($"You attacked the enemy for {playerDamage1}");
-                Console.WriteLine($"The enemy now has {enemyHealth} health");
-                playerHealth = enemyHealth - enemyDamage;
-                Console.WriteLine($"The enemy attacked you in retaliation, your health is now {playerHealth}");
-                answerfunc();
-                break;
+                default:
+                    Console.WriteLine("That was not one of the options");
+                    break;
+            }
 
-            default:
-                Console.WriteLine("That was not one of the options");
-                answerfunc();
-                break;
+            if (enemyHealth <= 0)
+            {
+                enemyHealth = 0;
+            }
+
+            if (playerHealth <= 0)
+            {
+                playerHealth = 0;
+            }
+
         }
 
 
     }
-
-    
-
 }
