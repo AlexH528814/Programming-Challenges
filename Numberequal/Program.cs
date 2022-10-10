e154a03b82ab8351119257a1c18f172317f00f89
528814@escg.ac.uk <528814@escg.ac.uk>
2022-10-10 13:16:31 +0100

    still dont know what I changed

---
+++ Numberequal/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+class Program
+{
+    static int num1;
+    static int num2;
+    static string answer1;
+    static string answer2;  
+    public static void Main(string[] args)
+    {
+
+        Console.WriteLine("Input your first number");
+        answer1 = Console.ReadLine();
+        num1 = Convert.ToInt32(answer1);
+        Console.WriteLine("");
+        Console.WriteLine("Input your second number");
+        answer2 = Console.ReadLine();
+        num2 = Convert.ToInt32(answer2);
+
+
+        if (num1 == num2)
+        {
+            Console.WriteLine("Both numbers are equal");
+        }
+
+        else
+        {
+            Console.WriteLine("Neither number is equal");
+        }
+
+    }
+}
