84555648369333f7e53f0077ff060f4f6f0ef23f
528814@escg.ac.uk <528814@escg.ac.uk>
2022-10-10 16:52:14 +0100

    challenges

---
+++ Adding2Numbers/Program.cs
@@ -0,0 +1,82 @@
+﻿using System;
+class Program
+{
+    //Add numbers function
+    static int Addnumbers(int x, int y)
+    {
+        int newValue = x + y;
+        return newValue;
+    }
+
+    static void Main()
+    {
+        Console.WriteLine(Addnumbers(4, 9));
+
+        int a = Convert.ToInt32(Console.ReadLine());
+        int b = Convert.ToInt32(Console.ReadLine());
+
+        Console.WriteLine(a + b);
+
+
+
+        //Superman vs Batman if else
+        Console.WriteLine("Who is your favourite superhero, batman or superman");
+        string hero = Console.ReadLine();
+
+        if (hero == "Superman" || hero == "superman")
+        {
+            Console.WriteLine("you are lame");
+        }
+
+        else if (hero == "Batman" || hero == "batman")
+        {
+            Console.WriteLine("you are cool");
+        }
+
+        else
+        {
+            Console.WriteLine("They dont exist (I'm not gaslighting you)");
+        }
+
+
+        //Superman vs Batman Switch
+        string hero2 =Console.ReadLine();
+        switch (hero2.ToLower())
+        {
+            case "saitama":
+                Console.WriteLine("One Punch");
+                break;
+
+            case "spiderman":
+                Console.WriteLine("Man like a spider");
+                break;
+
+            case "hawkeye":
+                Console.WriteLine("guy with a bow an arrow");
+                break;
+
+            case "hindsight lad":
+                Console.WriteLine("literally a guy who hashindsight");
+                break;
+
+            case "kiteman":
+                Console.WriteLine("guy with a kite");
+                break;
+
+            default:
+                Console.WriteLine("They dont exist (I'm not gaslighting you)");
+                break;
+        }
+
+        //Call a function
+        hellofunction("Hello");
+
+    } 
+
+    //Call a Function
+    static void hellofunction(string f)
+    {       
+         f = f + "s";
+        Console.WriteLine(f);
+    }
+}
---
+++ Challenges - Console Work/Program.cs
@@ -0,0 +1,106 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        Random random = new Random();
+        int playerDamage1 = random.Next(10, 15);
+        int playerDamage2 = random.Next(15, 20);
+        int playerDamage3 = random.Next(5, 10);
+        int playerDamage4 = random.Next(20, 25);
+        int enemyDamage = random.Next(10, 15);
+
+        int playerHealth = 150;
+        int enemyHealth = 150;
+
+
+
+        Console.WriteLine("You encountered an enemy, press C, R, Y or F to attack it");
+
+        answerfunc();
+
+        if (enemyHealth <= 0)
+        {
+            Console.WriteLine("You defeated the enemy");
+            Environment.Exit(0);
+        }
+
+        if (playerHealth <= 0)
+        {
+            Console.WriteLine("The enemy defeated you");
+            Environment.Exit(0);
+        }
+
+
+    }
+
+
+    static void answerfunc()
+    {
+        Random random = new Random();
+        int playerDamage1 = random.Next(10, 15);
+        int playerDamage2 = random.Next(15, 20);
+        int playerDamage3 = random.Next(5, 10);
+        int playerDamage4 = random.Next(20, 25);
+        int enemyDamage = random.Next(10, 15);
+        int playerHealth = 150;
+        int enemyHealth = 150;
+
+
+        var input = Console.ReadKey();
+        Console.WriteLine("");
+        switch (input.Key)
+        {
+            case ConsoleKey.C:
+                
+                enemyHealth = enemyHealth - playerDamage1;
+                Console.WriteLine($"You attacked the enemy for {playerDamage1}");
+                Console.WriteLine($"The enemy now has {enemyHealth} health");
+                playerHealth = enemyHealth - enemyDamage;
+                Console.WriteLine($"The enemy attacked you in retaliation, your health is now {playerHealth}");
+                answerfunc();
+                break;
+
+            case ConsoleKey.R:
+               
+                enemyHealth = enemyHealth - playerDamage2;
+                Console.WriteLine($"You attacked the enemy for {playerDamage1}");
+                Console.WriteLine($"The enemy now has {enemyHealth} health");
+                playerHealth = enemyHealth - enemyDamage;
+                Console.WriteLine($"The enemy attacked you in retaliation, your health is now {playerHealth}");
+                answerfunc();
+                break;
+
+            case ConsoleKey.Y:
+                
+                enemyHealth = enemyHealth - playerDamage3;
+                Console.WriteLine($"You attacked the enemy for {playerDamage1}");
+                Console.WriteLine($"The enemy now has {enemyHealth} health");
+                playerHealth = enemyHealth - enemyDamage;
+                Console.WriteLine($"The enemy attacked you in retaliation, your health is now {playerHealth}");
+                answerfunc();
+                break;
+
+            case ConsoleKey.F:
+              
+                enemyHealth = enemyHealth - playerDamage4;
+                Console.WriteLine($"You attacked the enemy for {playerDamage1}");
+                Console.WriteLine($"The enemy now has {enemyHealth} health");
+                playerHealth = enemyHealth - enemyDamage;
+                Console.WriteLine($"The enemy attacked you in retaliation, your health is now {playerHealth}");
+                answerfunc();
+                break;
+
+            default:
+                Console.WriteLine("That was not one of the options");
+                answerfunc();
+                break;
+        }
+
+
+    }
+
+    
+
+}
