724cc86775450d942da2b2b7df89e67af58bb9d5
Alex Huddle <528814@escg.ac.uk>
2022-10-17 14:35:55 +0100

    functions challenge 1.3

--- Functions 1.3/Program.cs
+++ Functions 1.3/Program.cs
@@ -1,15 +1,54 @@
 ﻿using System;
+using System.ComponentModel;
 
 class Program
 {
-    static void FourInt(int f, int g, int h, int j)
+    static int total;
+    public static void Main()
     {
-        Console.WriteLine(f + g + h + j);
+        Console.WriteLine("Give me 4 numbers");
+        totalint(Convert.ToInt32(Console.ReadLine()), Convert.ToInt32(Console.ReadLine()), Convert.ToInt32(Console.ReadLine()), Convert.ToInt32(Console.ReadLine()));
+
+
+    }
+    public static void totalint(int f, int g, int h, int j)
+    {
+        int total = f + g + h + j;
+        Console.WriteLine(total);
+
+        evenbig(total);
     }
 
-
-    static void Main()
+    public static void evenbig(int i)
     {
+        bool bignum;
 
+
+        if (i % 2 != 0)
+        {
+            Console.WriteLine("The number was not even so I added 1 to it");
+            i = i + 1;
+            Console.WriteLine(i);
+        }
+
+        else if (i % 2 == 0)
+        {
+            Console.WriteLine("the number was even so I kept it the same");
+            Console.WriteLine(i);
+        }
+
+        if (i >= 10)
+        {
+            bignum = true;
+            Console.WriteLine("The number was greater than 10, so the bool is");
+            Console.WriteLine(bignum);
+        }
+
+        else if (i < 10)
+        {
+            bignum = false;
+            Console.WriteLine("The number was smaller than 10, so the bool is");
+            Console.WriteLine(bignum);
+        }
     }
 }
