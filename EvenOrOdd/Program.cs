6362b3119255ac85550e44aac7bc21d094b1fcef
528814@escg.ac.uk <528814@escg.ac.uk>
2022-10-17 13:14:22 +0100

    donme

---
+++ EvenOrOdd/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        
+
+
+        int a = Convert.ToInt32(Console.ReadLine());
+
+        if (a % 2 == 0)
+        {
+            Console.WriteLine($"The number {a} is even");
+
+          
+        }
+
+        else
+        {
+            Console.WriteLine($"The number {a} is odd");
+
+           
+        }
+    }
+}
---
+++ Functions 1.3/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+class Program
+{
+    static void FourInt(int f, int g, int h, int j)
+    {
+        Console.WriteLine(f + g + h + j);
+    }
+
+
+    static void Main()
+    {
+
+    }
+}
