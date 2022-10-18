e846e103b91f3840316a09482a420b41e2b8480d
AlexH528814 <528814@escg.ac.uk>
2022-10-18 22:54:51 +0100

    prime numbers

---
+++ PrimeNumbers/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+class Program
+{
+    static void Main()
+    {
+        //Declaration  
+        bool isPrime = true;
+        int i; 
+        int j = 2;
+        //Calculate and display the Prime number  
+        Console.WriteLine("Prime Numbers are : ");
+        for (i = 2; i >= j; i++)
+        {
+            for (j = 2; j <= i; j++)
+            { 
+                Console.WriteLine(j);
+                   
+                if (i != j && i % j == 0)
+                {
+                   
+                    isPrime = false;
+                    break;
+                }
+            }
+            if (isPrime)
+            {
+                Console.WriteLine(i);
+            }
+            isPrime = true;
+        }
+        Console.ReadKey();
+    }
+}
