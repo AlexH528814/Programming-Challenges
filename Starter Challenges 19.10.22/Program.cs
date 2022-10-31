5e075c4530d110c490475d70ca2ac3c43480daf7
Alex Huddle <528814@escg.ac.uk>
2022-10-31 12:43:00 +0000

    idk

---
+++ Starter Challenges 19.10.22/Program.cs
@@ -0,0 +1,61 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        Quadrat();
+        Factors();
+    }
+
+    static void Factors()
+    {
+        Console.WriteLine("give me a number and I'll tell you its factors");
+        int x = Convert.ToInt32(Console.ReadLine());
+        for (int i = 2; i < x; i++)
+        {
+            if (x % i == 0)
+            {
+                Console.WriteLine(i);
+            }
+
+
+
+        }
+    }
+
+    static void Quadrat()
+    {
+
+        Console.WriteLine("Give me 2 coordinates");
+        int xcoord = Convert.ToInt32(Console.ReadLine());
+        int ycoord = Convert.ToInt32(Console.ReadLine());
+
+        if (xcoord == 0 && ycoord == 0)
+        {
+            Console.WriteLine($"The coordinate point ({xcoord},{ycoord}) lies in the origin");
+        }
+
+        else if (xcoord >= 0 && ycoord >= 0)
+        {
+            Console.WriteLine($"The coordinate point ({xcoord},{ycoord}) quadrat 1");
+        }
+
+        else if (xcoord <= 0 && ycoord <= 0)
+        {
+            Console.WriteLine($"The coordinate point ({xcoord},{ycoord}) quadrat 3");
+        }
+
+        else if (xcoord >= 0 && ycoord <= 0)
+        {
+            Console.WriteLine($"The coordinate point ({xcoord},{ycoord}) quadrat 4");
+        }
+
+        else if (xcoord <=  0 && ycoord >= 0)
+        {
+            Console.WriteLine($"The coordinate point ({xcoord},{ycoord}) quadrat 2");
+        }
+
+        
+    }
+}
