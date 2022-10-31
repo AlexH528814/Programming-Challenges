e3593ae6094204ab8c9feaabf7375e68d1f0ae05
Alex Huddle <528814@escg.ac.uk>
2022-10-31 15:34:31 +0000

    done

--- Functions 1.3/Program.cs
+++ Functions 1.3/Program.cs
@@ -6,23 +6,34 @@ class Program
    
     public static void Main()
     {
+       
+       
         Console.WriteLine("Give me 4 numbers");
-        totalint(Convert.ToInt32(Console.ReadLine()), Convert.ToInt32(Console.ReadLine()), Convert.ToInt32(Console.ReadLine()), Convert.ToInt32(Console.ReadLine()));
+        int a = Convert.ToInt32(Console.ReadLine());
+        int b = Convert.ToInt32(Console.ReadLine());
+        int c = Convert.ToInt32(Console.ReadLine());
+        int d = Convert.ToInt32(Console.ReadLine());
+        int total = totalint(a, b, c, d);;
+      
+        bool evenodd = evenbig(total);
+        Console.WriteLine(evenodd);
+        
     }
     public static int totalint(int a, int b, int c, int d)
     {
         int total = a + b + c + d;
+        Console.WriteLine(total);
         return total;
 
-        evenbig(total);
+     
         
     }
 
-    public static bool evenbig(int i)
+    public static bool evenbig(int total)
     {
-        if (i % 2 != 0)
+        if (total % 2 != 0)
         {         
-            i = i + 1;
+            total = total + 1;
             return false;
         }
 
