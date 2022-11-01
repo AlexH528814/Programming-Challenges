963ff9f3f1bfe6cc39c385ba930178c829b231d5
AlexH528814 <528814@escg.ac.uk>
2022-11-01 19:12:49 +0000

    fizzbuzz

---
+++ FizzBuzz/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        Fizzbuzz();
+    }
+
+    static void Fizzbuzz()
+    {
+
+        for (int i = -50; i < 100; i++)
+        {           
+            if (i % 5 == 0 && i % 3 == 0)
+            {
+                Console.WriteLine("Fizzbuzz");
+            }
+            else if (i % 5 == 0)
+            {
+                Console.WriteLine("Buzz");
+            }
+            else if (i % 3 == 0)
+            {
+                Console.WriteLine("Fizz");
+            }
+            else
+            {
+                Console.WriteLine(i);
+            }
+        }
+
+    }
+}
