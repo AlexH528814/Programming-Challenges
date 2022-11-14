58c4e67d09d7fa66723f1f59243577e2b00d3457
Alex Huddle <528814@escg.ac.uk>
2022-11-14 12:43:28 +0000

    s

--- Coding Challenege 4.2/Program.cs
+++ Coding Challenege 4.2/Program.cs
@@ -1,2 +1,16 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+using System.ComponentModel;
+using System.Net.Http.Headers;
+using System.Reflection;
+using System.Runtime.ConstrainedExecution;
+
+
+
+class Program
+{
+    static void Main()
+    {
+        Game.Start();
+        Environment.Exit(0);
+    }
+}
---
+++ Stars and Pyramids/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        int spaces = Convert.ToInt32(Console.ReadLine());
+        int stars = Convert.ToInt32(Console.ReadLine());
+
+        Drawstars(spaces, stars);
+
+    }
+
+    static void Drawstars(int spaces, int stars)
+    {
+        for (int i = 0; i <= spaces; i++)
+        {
+            Console.Write(" ");
+        }
+
+        for (int i = 0; i <= stars; i++)
+        {
+            Console.Write("*");
+        }
+        Console.WriteLine("");
+
+
+
+    }
+}
