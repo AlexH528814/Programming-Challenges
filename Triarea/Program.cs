aee5f2a9774858cc4d48c8592fc01ad5c8211954
Alex Huddle <528814@escg.ac.uk>
2022-11-16 15:27:11 +0000

    more stuff

---
+++ Triarea/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+class Program
+{
+
+    public static void Main()
+    {
+        int tribase = 4;
+        int height = 5;
+
+        triArea(tribase, height);
+    }
+
+    public static void triArea(int tribase, int height)
+    {
+        Console.WriteLine(tribase * height / 2);
+    }
+
+}
+
