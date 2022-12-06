817e89c6c65dc80cf6c90cd64aa44a740a6c3756
Alex Huddle <528814@escg.ac.uk>
2022-12-06 16:14:05 +0000

    arrays

---
+++ Arrays/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+class Program
+{
+    static string[] cars = { "Volvo", "BMW", "Ford", "Mazda" };
+
+    static void Main()
+    {
+        cars[0] = "Opel";
+
+        Array.Sort(cars);
+
+        foreach (string car in cars)
+        {
+            Console.WriteLine(car);
+        }
+    }
+}
---
+++ Golden Ratio/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+class Program
+{
+    static int x = 0;
+    static int y = 0;
+    static int n;
+    static void Main()
+    {
+
+        Console.WriteLine(Fibonacci(Convert.ToInt32(Console.ReadLine())));
+
+
+    }
+
+    public static long Fibonacci(int num)
+    {
+        long a = 0;
+        long b = 1;
+
+        for (int i = 0; i < num; i++)
+        {
+            long temp = a;
+            a = b;
+            b = temp + b;
+            Console.WriteLine(a);
+        }
+        return a;
+    }
+}
